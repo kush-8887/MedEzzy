ced97931dd722045258311c4cefa8bae45595a8e
Kush Jaiswal <jaiswalkush8@gmail.com>
2024-03-29 23:51:26 +0530

    Made UI changes

--- public/js/utils/logout.js
+++ public/js/utils/logout.js
@@ -1,16 +1,20 @@
-const logoutBtn = document.getElementById('logoutBtn');
-
 function deleteCookie(name) {
+    console.log(document.cookie);
     document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
 }
 
-if (!logoutBtn) {
-    console.log("Logout btn not found!");
-} else {
-    logoutBtn.addEventListener('click', (e) => {
-        e.preventDefault();
+document.addEventListener('DOMContentLoaded',()=>{
+    const logoutBtn = document.getElementById('logoutBtn');
 
-        deleteCookie("access-token");
-        window.location.reload();
-    });
-}
+    if (!logoutBtn) {
+        console.log("Logout btn not found!");
+    } else {
+        logoutBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+    
+            deleteCookie("access-token");
+            window.location.reload();
+        });
+    }
+})
+
